a23581dac5e173e7f3d5f52f41e8c5ed3a89b75e
mherrera01 <miguelherrera8600@gmail.com>
2023-01-18 16:05:27 +0100

    Fruit pick list functionality added

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,7 +16,10 @@ my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.co
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
 # Create a pick list so the users can pick the fruit they want to include 
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado', 'Strawberries'])
+
+# Filter the fruit list depending on user input
+fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 # Display the table on the page
-streamlit.dataframe(my_fruit_list)
+streamlit.dataframe(fruits_to_show)
