8bf74275e13d37bad7bb1eda4e15ce28a15456ed
mherrera01 <miguelherrera8600@gmail.com>
2023-01-18 15:49:33 +0100

    Breakfast menu text

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
