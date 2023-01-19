c794a812cdcad0b2e4e496b810c9fc1d34264599
mherrera01 <miguelherrera8600@gmail.com>
2023-01-19 11:46:41 +0100

    Fruityvice API input error handled

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@ import streamlit
 import pandas
 import requests
 import snowflake.connector
+from urllib.error import URLError
 
 streamlit.title('My Parents New Healthy Diner')
 
@@ -28,17 +29,23 @@ streamlit.dataframe(fruits_to_show)
 
 # New section for the fruityvice API
 streamlit.header("Fruityvice Fruit Advice!")
+try:
+    # Request user input for getting the API response
+    fruit_choice = streamlit.text_input('What fruit would you like information about?')
+    if not fruit_choice:
+        streamlit.error("Please select a fruit to get information.")
+    else:
+        # Get API response
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 
-# Request user input for getting the API response
-fruit_choice = streamlit.text_input('What fruit would you like information about?', 'Kiwi')
-streamlit.write('The user entered ', fruit_choice)
+        # Normalize the JSON API response and display it as a table
+        fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+        streamlit.dataframe(fruityvice_normalized)
 
-# Get API response
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+except URLError as e:
+    streamlit.error()
 
-# Normalize the JSON API response and display it as a table
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-streamlit.dataframe(fruityvice_normalized)
+streamlit.stop()
 
 # Connect to the snowflake account
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
@@ -55,3 +62,5 @@ streamlit.dataframe(my_data_row)
 # Request user input for adding a fruit to the list
 add_my_fruit = streamlit.text_input('What fruit would you like to add?')
 streamlit.write('Thanks for adding ', add_my_fruit)
+
+my_cur.execute("insert into fruit_load_list values ('from streamlit')")
