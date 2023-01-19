9554b89bd1a485fd03f084c4a124048eca1a93d1
mherrera01 <miguelherrera8600@gmail.com>
2023-01-19 09:14:56 +0100

    API response based on input implemented

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,8 +28,12 @@ streamlit.dataframe(fruits_to_show)
 # New section for the fruityvice API
 streamlit.header("Fruityvice Fruit Advice!")
 
+# Request user input for getting the API response
+fruit_choice = streamlit.text_input('What fruit would you like information about?', 'Kiwi')
+streamlit.write('The user entered ', fruit_choice)
+
 # Get API response
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "watermelon")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 
 # Normalize the JSON API response and display it as a table
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
